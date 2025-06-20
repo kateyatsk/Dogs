1232b10831b7871b8f07e7a2073d4a96fa245f10
kateyatsk <klaettoe2023@gmail.com>
2025-06-20 13:20:25 +0300

    Initial Commit

---
+++ Dogs/DogsApp.swift
@@ -0,0 +1,17 @@
+//
+//  DogsApp.swift
+//  Dogs
+//
+//  Created by Екатерина Яцкевич on 20.06.25.
+//
+
+import SwiftUI
+
+@main
+struct DogsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
